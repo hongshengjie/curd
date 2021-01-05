cbcac2df9614c3a5b9ae0ae2cb08f38cf84bf816
hongshengjie <hongshengjie@bilibili.com>
2021-01-05 11:14:00 +0800

    add

--- example/main.go
+++ example/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"time"
 
 	"database/sql"
 	"encoding/json"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/hongshengjie/crud/example/alltypetable"
 	"github.com/hongshengjie/crud/example/user"
 )
 
@@ -24,12 +26,14 @@ func InitDB() {
 }
 func main() {
 	InitDB()
+	//createAllType()
+	updateAllType()
 	//create()
-	findOne()
-	find()
-	update()
-	count()
-	tx()
+	//findOne()
+	//find()
+	//updateUser()
+	//count()
+	//tx()
 	db.Close()
 
 }
@@ -48,13 +52,67 @@ func findOne() {
 
 func find() {
 	user, err := user.Find(ctx, db, user.Query(user.
-		Where().NotIn(user.ID, []interface{}{1, 2})).OrderDesc(user.ID).Limit(2).Offset(2))
+		Where().
+		NotIn(user.ID, []interface{}{1, 2})).
+		OrderDesc(user.ID).
+		OrderAsc(user.Age).
+		Limit(2).Offset(2))
 	b, _ := json.Marshal(user)
 	fmt.Printf("%s, %+v \n", string(b), err)
 }
 
-func update() {
-	user.Update(user.Where().Eq(user.ID, 1)).SetAge(1).SetName("java").Save(ctx, db)
+func updateUser() {
+	eff, err := user.Update(user.Where().Eq(user.ID, 1)).
+		SetAge(10).
+		SetName("python").
+		SetCtime(time.Now()).
+		Save(ctx, db)
+	fmt.Println(eff, err)
+}
+
+func updateAllType() {
+	ef, err := alltypetable.Update(alltypetable.Where().Eq(alltypetable.ID, 1)).
+		SetBInt(11).SetBinaryM([]byte("java")).SetEnumM("n").SetSetM("c").Save(ctx, db)
+	fmt.Println(ef, err)
+
+}
+
+func createAllType() {
+	err := alltypetable.Insert(ctx, db, &alltypetable.AllTypeTable{
+		ID:              0,
+		TInt:            0,
+		SInt:            0,
+		MInt:            0,
+		BInt:            0,
+		F32:             0,
+		F64:             0,
+		DecimalMysql:    0,
+		CharM:           "",
+		VarcharM:        "",
+		JSONM:           "{}",
+		NvarcharM:       "",
+		NcharM:          "",
+		TimeM:           "23:59:59",
+		DateM:           time.Time{},
+		DataTimeM:       time.Time{},
+		TimestampM:      time.Time{},
+		TimestampUpdate: time.Time{},
+		YearM:           "2021",
+		TText:           "",
+		MText:           "",
+		TextM:           "",
+		LText:           "",
+		BinaryM:         []byte{},
+		BlobM:           []byte{},
+		LBlob:           []byte{},
+		MBlob:           []byte{},
+		TBlob:           []byte{},
+		BitM:            []uint8{},
+		EnumM:           "y",
+		SetM:            "a",
+		BoolM:           0,
+	})
+	fmt.Println(err)
 }
 
 func count() {
